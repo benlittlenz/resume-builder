dedb90b0d1d4d6c27fc460d1ec7f4b00e4503b1b
benlittlenz <benlittle53@gmail.com>
2021-06-19 21:18:20 +1200

    added modal to add new content

---
+++ src/components/AddContent.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export function AddContent() {
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+        <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-6xl sm:w-full sm:p-6">
+          <div className="grid grid-cols-3 gap-4">
+            <div className="flex items-center justify-center bg-gray-50 rounded-lg py-4 shadow-lg hover:bg-gray-100 hover:shadow-xl hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+              </svg>
+              <h2 className="font-semibold text-xl px-4">Education</h2>
+            </div>
+            <div className="flex items-center justify-center bg-gray-50 rounded-lg py-4 shadow-lg hover:bg-gray-100 hover:shadow-xl hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+              </svg>
+              <h3 className="font-semibold text-xl px-4">Skill</h3>
+            </div>
+            <div className="flex items-center justify-center bg-gray-50 rounded-lg py-4 shadow-lg hover:bg-gray-100 hover:shadow-xl hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              </svg>
+              <h3 className="font-semibold text-xl px-4">Professional Experience</h3>
+            </div>
+            <div className="flex items-center justify-center bg-gray-50 rounded-lg py-4 shadow-lg hover:bg-gray-100 hover:shadow-xl hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              </svg>
+              <h3 className="font-semibold text-xl px-4">Professional Experience</h3>
+            </div>
+            <div className="flex items-center justify-center bg-gray-50 rounded-lg py-4 shadow-lg hover:bg-gray-100 hover:shadow-xl hover:cursor-pointer">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              </svg>
+              <h3 className="font-semibold text-xl px-4">Professional Experience</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  )
+}
--- src/components/Builder.tsx
+++ src/components/Builder.tsx
@@ -6,18 +6,30 @@ import { useAppSelector } from '../app/hooks';
 
 import { Name } from './PersonsalDetails/Output/Name'
 import { Contact } from './PersonsalDetails/Output/Contact'
+import { AddContent } from './AddContent'
 export function Builder() {
+  const [showModal, setShowModal] = useState(false)
   const details = useAppSelector((state) => state.detailsForm)
 
   return (
-    <div className="flex items-center bg-gray-100">
-      <div className="h-screen w-4/12">
-      <PersonalDetails />
+    <div>
+      <div className="flex items-center bg-gray-100">
+        <div className="h-screen w-4/12">
+          <PersonalDetails />
+          <div className="flex justify-center w-full mx-auto">
+            <button
+              className="mt-6 bg-green-500 text-white text-lg font-semibold px-10 py-2 rounded-md"
+              onClick={() => setShowModal(true)}
+            >Add Content</button>
+          </div>
+
+        </div>
+        <div className="h-screen w-6/12 bg-white shadow-lg rounded-md mx-8 my-8 mx-auto">
+          <Name name={details.fullname} />
+          <Contact details={details} />
+        </div>
       </div>
-      <div className="h-screen w-6/12 bg-white shadow-lg rounded-md mx-8 my-8 mx-auto">
-        <Name name={details.fullname} />
-        <Contact details={details} />
-      </div>
+      {showModal && <AddContent />}
     </div>
   )
 }
