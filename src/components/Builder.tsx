345456d02a5ce7f3bd3e0a6a5e4c0847441147f7
benlittlenz <benlittle53@gmail.com>
2021-06-19 13:57:49 +1200

    setup redux toolkit

--- src/components/Builder.tsx
+++ src/components/Builder.tsx
@@ -4,8 +4,14 @@ import { PersonalDetails } from './PersonsalDetails'
 
 export function Builder() {
   return (
-    <div>
+    <div className="flex items-center">
+      <div className="w-4/12">
       <PersonalDetails />
+      </div>
+      <Counter />
+      <div className="h-screen w-6/12 bg-red-500 mx-8 my-8 mx-auto">
+sddfsdfsfd
+      </div>
     </div>
   )
 }
